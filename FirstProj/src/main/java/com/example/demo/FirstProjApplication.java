248b9c0b459d69f5d9850460cc2746a92ca7d045
Neerav Bhaskarla <neeravbhaskarla@Neeravs-MacBook-Air.local>
2022-06-23 19:11:22 +0530

    first commit

---
+++ FirstProj/src/main/java/com/example/demo/FirstProjApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class FirstProjApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(FirstProjApplication.class, args);
+		
+		Alien a1 = context.getBean(Alien.class);
+		a1.show();
+		
+		Alien a2 = context.getBean(Alien.class);
+		a2.show();
+	}
+
+}
